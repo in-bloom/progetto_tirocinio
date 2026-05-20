91f14c982a3f96423649644bd0915afa0b2df6fe
in-bloom <f.ricciardi9@studenti.unipi.it>
2026-05-20 17:48:59 +0200

    Update README and notebooks to reflect changes in DocBin processing; remove unused docbin file and add custom attributes for post_id and submolt in spacy_process.py

--- README.md
+++ README.md
@@ -12,6 +12,5 @@
 ## Step 3: SpaCy Pipeline
 
 - I post sono stati analizzati per ora con la pipeline standard del modello en_core_web_lg.
-- Ogni doc risultante dall'analisi viene poi inserito in un docbin che viene salvato nella cartella data.
-- **Problemi:** il formato docbin salva perfettamente le annotazioni di spacy ma pare non permetta di salvare le estensioni aggiunte al Doc (oggetto di spacy). Una opzione che ho trovato è usare un json a parte per mantenere la relazione tra i testi annotati e i dati in _processed_lang.parquet_. Altra opzione potrebbe essere di salvare i risultati in forma tabulare e relazionale.
+- Ogni doc risultante dall'analisi viene poi inserito in un docbin con il suo id e il submolt di appartenenza che viene salvato nella cartella specificata.
 - Nel secondo notebook ho effettuato dei test per capire come funziona il formato e come ottenere le annotazioni di spacy. Sono presenti la lunghezza media dei post in token e la ricchezza lessicale media nel dataset.
--- src/spacy_process.py
+++ src/spacy_process.py
@@ -1,5 +1,6 @@
 import spacy
-from spacy.tokens import DocBin
+from spacy.tokens import DocBin, Doc
+# from spacytextblob.spacytextblob import SpacyTextBlob
 import pandas as pd
 from tqdm import tqdm
 
@@ -7,22 +8,32 @@ from tqdm import tqdm
 # ancora da definire se la pipeline standard va bene o da modificare
 # I doc vengono aggiungi ad un DocBin unico che poi viene salvato 
 # tra i dati per le analisi con i notebook
+# Custom attributes: post_id e submolt
 
-# ?
-# Se necessario mantenere la relazione tra i dati in parquet e il testo processato
-# si può fare docbin + json oppure salvare i vari risultati in parquet.
-# Valutare come salvare e gestire le informazioni
-# ?
+if not Doc.has_extension("post_id"):
+    Doc.set_extension("post_id", default=None)
+if not Doc.has_extension("submolt"):
+    Doc.set_extension('submolt', default=None)
+
 nlp = spacy.load('en_core_web_lg')
 df = pd.read_parquet('./data/processed/english_data.parquet')
 
-doc_bin = DocBin()
+# Non è serializzabile nel docbin, si può aggiungere come custom attribute ma rimuovendo la pipe prima di salvare il documento:
+# Possibile opzioni:
+# - Fare una lista che contiene i documenti, rimuovere la pipe e aggiungere con custom attr.
+# - Salvare i risultati in modo diverso o in un altro script/notebook.
+# nlp.add_pipe('spacytextblob')
 
-for doc in tqdm(
-    nlp.pipe(df['content']),
+doc_bin = DocBin(store_user_data=True)
+
+for doc, row in tqdm(
+    zip(nlp.pipe(df['content']), df.itertuples()),
     total=len(df)
 ):
+    doc._.post_id = row.id
+    doc._.submolt = row.submolt
     doc_bin.add(doc)
 
-doc_bin.to_disk('./data/processed/docs_prova.docbin')
+
+doc_bin.to_disk('./data/processed/annotated_documents.spacy')
 print('documents processed and saved')
